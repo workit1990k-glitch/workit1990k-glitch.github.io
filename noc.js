7bfa2ab9aa744cca6982742e3f8ddd671f24fe29
workit1990k-glitch <workit1990k@gmail.com>
2026-05-04 22:49:34 +0300

    Update noc.js

--- noc.js
+++ noc.js
@@ -7,7 +7,7 @@
     imgQuality: 0.75, 
     parallelFetch: 3,
     selectors: { 
-      title: '.title', 
+      title: '.novel_subtitle', 
       content: '.p-novel__body' 
     }, 
     epubLang: 'ja',
